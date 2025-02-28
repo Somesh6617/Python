82762e95d70b25fad581b19c4483cda3703dc8b4
Somesh6617 <someshwar6617@gmail.com>
2025-02-28 22:47:05 +0530

    Add files via upload

---
+++ multiple_inputs.py
@@ -0,0 +1,11 @@
+#split() keyword used to take multiple inputs at a time
+#a,b,c=input("enter the numbers ").split(" ") 
+        # or 
+d=input("enter the numbers ")
+a,b,c=d.split(" ")
+x=int(a)#converting from string to integer
+y=int(b)#converting from string to integer
+z=int(c)#converting from string to integer
+print(a,b,c)#prints string
+print(a+b+c)#prints string using concatination(+)
+print(x+y+z)#prints sum of x,y,z
