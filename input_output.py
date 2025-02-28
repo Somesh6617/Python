049f51dd957d2c500fa97399fd3e6ddf7b62f1c3
Somesh6617 <someshwar6617@gmail.com>
2025-02-28 22:42:43 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,27 @@
+"""number=int(input("enter any number "))
+print(type(number),"your entered value is :",number)
+decimal=float(input("enter any float value "))
+print(type(decimal),"your entered value is:",decimal)
+string=input("enter any word/sentence ")
+print(type(string),"your entered string is:",string5)
+name=input("enter your name ")
+age=int(input("enter your age "))
+print("your name is "+ name +"and you are ",age ,"years old")
+n1=int(input("enter any value "))
+n2=int(input("enter any value "))
+print(n1,n2,n1,n2,n1,n2,sep=" & ",end=" your ended")
+name=int(input("enter value "))
+print("You entered:",name,sep="")
+pi=3.14
+print("pI value is",pi)
+a=input()
+x,y,z=a.split(" ")
+print(x,y,z)
+na=input("ener name ")
+name,age=na.split(" ")
+print(name,age)
+print("Name:",name,",Age:",age,sep="")"""
+a=input("enter values ") # it is string
+x,y,z=a.split(" ") # splitted to x,y,z
+sum=int(x)+int(y)+int(z) # converted into integer
+print(sum)
