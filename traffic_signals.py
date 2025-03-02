0ee849d0a972e8926e8685f8b6588235729ef01d
Somesh6617 <someshwar6617@gmail.com>
2025-03-02 22:56:15 +0530

    Add files via upload

---
+++ traffic_signals.py
@@ -0,0 +1,9 @@
+signal=input("Enter the signal:")
+if(signal=="red" or signal=="RED"):
+    print("please STOP the vechile")
+elif (signal=="yellow" or signal=="YELLOW"):
+    print("READY to go")
+elif (signal=="green" or signal=="GREEN"):
+    print("you can GO")
+else:
+    printa("Invalid signal")
