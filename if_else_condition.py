7d7e007bb8d5e6f0587fa97618d974fdefde92ce
Somesh6617 <someshwar6617@gmail.com>
2025-03-03 22:27:26 +0530

    Add files via upload

---
+++ if_else_condition.py
@@ -0,0 +1,7 @@
+#if the condition if false the code present in the else statement is executed
+num=int(input("enter the number: "))
+if num>10:
+    print("Hello")
+else :
+    print("Hi")
+print("code is done")
