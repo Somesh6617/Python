9db5cb90ec9e5951556276a9327d84456fb0f610
Somesh6617 <someshwar6617@gmail.com>
2025-03-06 21:39:07 +0530

    Add files via upload

---
+++ output_statements.py
@@ -0,0 +1,5 @@
+a=3
+b=18
+print(a,b)#prints a and b values separated with space(,)
+print(a,b,sep=",")#prints a and b values separated with comma(,)
+print(a,b,a,b,sep="*",end="!")#prints a and b values separated with s* and end with !
