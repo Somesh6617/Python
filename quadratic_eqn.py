8ae89a35bae234c481405f1e135be7f815aa852f
Somesh6617 <someshwar6617@gmail.com>
2025-03-06 21:39:30 +0530

    Add files via upload

---
+++ quadratic_eqn.py
@@ -0,0 +1,8 @@
+a=int(input("enter a value: "))
+b=int(input("enter b value: "))
+c=int(input("enter c value: "))
+x=-b
+y=((b**2)-4*a*c)**0.5
+z=2*a
+print(f"Root 1:{(x+y)/z}")
+print(f"Root 1:{(x-y)/z}")
