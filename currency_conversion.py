5fd38688390941351bcd66066fb89bb8fab47cbc
Somesh6617 <someshwar6617@gmail.com>
2025-03-06 21:38:10 +0530

    Add files via upload

---
+++ currency_conversion.py
@@ -0,0 +1,5 @@
+rupees=int(input("enter the indian rupee: "))
+one_doller=int(input("value of one doller in indian rupees: "))
+doller_rupees=rupees*one_doller
+rupees_doller=rupees/one_doller
+print(f"one rupee:{rupees},one doller:{one_doller},doller to rupees:{doller_rupees},rupees to doler:{rupees_doller}")
