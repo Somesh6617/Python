fc56348e0ce041858da8273528695f2197f55525
Somesh6617 <someshwar6617@gmail.com>
2025-03-11 22:08:33 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print("It\'s a beautiful day") #\'
+print("It\"s a beautiful day")#\""
+print("Hello \nworld")#\n
+print("Hello \tworld")#\t
+print("\u0042")  # Unicode for 'A'
+print("\U0001F601")  # Unicode for 😀 (grinning face emoji)
+print("\x41")  # Hexadecimal for 'A'
