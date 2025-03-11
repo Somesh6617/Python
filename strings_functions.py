2e2811ec47762f8e425a233e2827958141c9b600
Somesh6617 <someshwar6617@gmail.com>
2025-03-11 22:09:13 +0530

    Add files via upload

---
+++ strings_functions.py
@@ -0,0 +1,16 @@
+str="hello world"
+#string slicing
+print(str[0])
+print(str[3])
+print(str[-1]) 
+print(str[::-1])
+print(str[2::-1])
+print(str[:2:-1])
+#string concatenation
+n1="donthu"
+n2="someshwar"
+n3=n1+" "+n2
+print(n1+" "+n2)
+print(n3)
+#string length
+print(len(n3))
