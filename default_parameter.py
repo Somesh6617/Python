f23a15cae479a36ad133a355721ad6751b9844ff
Somesh6617 <someshwar6617@gmail.com>
2025-04-27 22:39:25 +0530

    Add files via upload

---
+++ default_parameter.py
@@ -0,0 +1,6 @@
+#if no arguments pass through a function definition the default value provided to a parameters
+def name(n1,n2):
+    print(n1+" "+n2)
+n1=input("enter name1: ")
+n2=input("enter name2: ")
+names=name(n1,n2="nani")#n2 already takes a input but it uses "vasu" because it placed in argument
