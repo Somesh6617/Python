02ee79c937103f16afecdc220e8957fae02a1e1d
Somesh6617 <someshwar6617@gmail.com>
2025-07-24 22:47:06 +0530

    Add files via upload

--- slicing.py
+++ slicing.py
@@ -1,8 +1,11 @@
-names=[1,2,3,4,5,6,7]
-print(names[:])
-print(names[1:9])
-print(names[0:])
-print(names[0:7:2])
-print(names[::-2])
-print(names[0:8:3])
-print(names[1:6:1])
+#it is process of accessing the portion of sequence of items(char,list,tuple etc).
+name="someshwar donthu"  #[start:stop:step]
+print(name[:9])
+print(name[10:])
+print(name[::-1])
+print(name[::2])
+
+name1=slice(0,10) # it is not a keyword it is a built in function.
+print(name[name1])
+string=""
+print(len(string))
