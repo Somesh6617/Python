54355bdc91b02c0a48339f39e3b78a2165aaaba6
Somesh6617 <someshwar6617@gmail.com>
2025-07-24 22:46:18 +0530

    Add files via upload

---
+++ math_functions.py
@@ -0,0 +1,11 @@
+import math
+x=1.5
+y=3
+print(round(x))
+print(math.ceil(x))
+print(math.floor(x))
+print(math.sqrt(226))
+print(abs(x)) # returns absolute value
+print(pow(y,2))
+print(max(x,y))
+print(min(x,y))
