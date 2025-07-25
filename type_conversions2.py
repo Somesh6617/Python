d222142255849fc86f8acf7cb161cb08fb1a0358
Somesh6617 <someshwar6617@gmail.com>
2025-07-25 22:00:19 +0530

    Add files via upload

---
+++ type_conversions2.py
@@ -0,0 +1,8 @@
+name='My name is vasu'
+age=20
+#print(name+" "+"i am",age,"years old")
+#print(name+" i am "+str(age),"years old")
+#print(type(age))
+val=True
+print("My name is vasu "+str(val))
+print("My name is vasu",val)
